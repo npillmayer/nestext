f79462420f5e3334a542aafad964cff7be868c4d
Norbert Pillmayer <norbert@pillmayer.com>
2021-11-07 15:05:04 +0100

    Completed example output

--- example_test.go
+++ example_test.go
@@ -1,6 +1,7 @@
 package nestext_test
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/npillmayer/nestext"
@@ -10,19 +11,24 @@ func ExampleParse() {
 	address := `
 name: Katheryn McDaniel
 address:
-	> 138 Almond Street
-	> Topeka, Kansas 20697
+  > 138 Almond Street
+  > Topeka, Kansas 20697
 phone:
-	cell: 1-210-555-5297
-	home: 1-210-555-8470
-		# Katheryn prefers that we always call her on her cell phone.
+  cell: 1-210-555-5297
+  home: 1-210-555-8470
+    # Katheryn prefers that we always call her on her cell phone.
 email: KateMcD@aol.com
 additional roles:
-	- board member
+  - board member
 `
-	_, err := nestext.Parse(strings.NewReader(address))
+
+	// parse address input and print out e-mail item
+	result, err := nestext.Parse(strings.NewReader(address))
 	if err != nil {
 		panic(err)
 	}
+	m := result.(map[string]interface{})
+	fmt.Printf("Katheryn's e-mail: %v\n", m["email"])
 	// Output:
+	// Katheryn's e-mail: KateMcD@aol.com
 }
