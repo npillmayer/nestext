c98d321ce1538b65f6e3418aeec35f1b00264013
Norbert Pillmayer <norbert@pillmayer.com>
2021-11-15 11:09:02 +0100

    started working on NT test-suite cases

---
+++ testsuite/suite_test.go
@@ -0,0 +1,214 @@
+package testsuite_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/npillmayer/nestext"
+	"github.com/npillmayer/nestext/ntenc"
+)
+
+var suitePath = filepath.Join(".", "official_tests", "test_cases")
+
+func casePath(c *ntTestCase) string {
+	return filepath.Join(suitePath, c.name)
+}
+
+func caseFilePath(c *ntTestCase, f string) string {
+	return filepath.Join(casePath(c), f)
+}
+
+type ntTestCase struct {
+	name    string
+	dir     fs.FileInfo
+	files   []fs.FileInfo
+	data    map[string][]byte
+	isLoad  bool
+	isDump  bool
+	status  string
+	statusD string
+	statusE string
+	isFail  bool
+}
+
+func (c ntTestCase) contains(f string) bool {
+	for _, a := range c.files {
+		if a.Name() == f {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *ntTestCase) bytes(f string) []byte {
+	r, err := os.Open(caseFilePath(c, f))
+	if err != nil {
+		return nil
+	}
+	b, err := ioutil.ReadAll(r)
+	r.Close()
+	if err != nil {
+		return nil
+	}
+	return b
+}
+
+func ttype(name string) string {
+	if strings.Contains(name, "string") {
+		return "string"
+	}
+	if strings.Contains(name, "list") {
+		return "list"
+	}
+	if strings.Contains(name, "dict") {
+		return "dict"
+	}
+	if strings.Contains(name, "holistic") {
+		return "dict"
+	}
+	return ""
+}
+
+func TestAll(t *testing.T) {
+	cases := listTestCases(t)
+	min, max := 0, 102
+	for i, c := range cases[min : max+1] {
+		if strings.HasPrefix(c.name, ".") {
+			cases[min+i].status = "-"
+			continue
+		}
+		runTestCase(&cases[min+i], t)
+	}
+	failcnt := 0
+	for i, c := range cases[min : max+1] {
+		t.Logf("test (%03d) %-21q: %10s  [ %-7s , %-7s ]", min+i, c.name, c.status, c.statusD, c.statusE)
+		if c.isFail {
+			failcnt++
+		}
+	}
+	t.Logf("%d out of %d tests failed", failcnt, len(cases))
+}
+
+func runTestCase(c *ntTestCase, t *testing.T) {
+	if err := loadTestCase(c, t); err != nil {
+		c.status = fmt.Sprintf("error: %s", err.Error())
+		c.isFail = true
+		t.Errorf("cannot open %q, skipping", c.name)
+	}
+	testDecodeCase(c, t)
+	testEncodeCase(c, t)
+}
+
+func loadTestCase(c *ntTestCase, t *testing.T) (err error) {
+	if c.files, err = ioutil.ReadDir(casePath(c)); err != nil {
+		return
+	}
+	c.status = "located"
+	c.data = make(map[string][]byte)
+	t.Logf("case.name = %q, %d files", c.name, len(c.files))
+	for _, fname := range []string{
+		"load_in.nt", "load_out.json", "load_err.json",
+		"dump_in.json", "dump_out.nt", "dump_err.json",
+	} {
+		if c.contains(fname) {
+			c.data[fname] = c.bytes(fname)
+		}
+	}
+	if _, ok := c.data["load_in.nt"]; ok {
+		c.isLoad = true
+	}
+	if _, ok := c.data["dump_in.json"]; ok {
+		c.isDump = true
+	}
+	c.status = "loaded"
+	return
+}
+
+func testDecodeCase(c *ntTestCase, t *testing.T) {
+	t.Logf("decoding-test %q", c.name)
+	if c.isLoad {
+		b := c.data["load_in.nt"]
+		//t.Logf("data_in = %q", string(b))
+		nt, err := nestext.Parse(bytes.NewReader(b))
+		t.Logf("done")
+		if err != nil {
+			if c.contains("load_err.json") {
+				c.statusD = "ok"
+				return
+			}
+			c.statusD = err.Error()
+			c.isFail = true
+			return
+		}
+		c.statusD = "parsed"
+		if compareOutput(nt, c, t) {
+			c.statusD = "ok"
+		}
+	}
+}
+
+func testEncodeCase(c *ntTestCase, t *testing.T) {
+	t.Logf("encoding-test %q", c.name)
+	if c.isDump {
+		b := c.data["dump_in.json"]
+		var r interface{}
+		switch ttype(c.name) {
+		case "dict":
+			r = make(map[string]interface{})
+		case "list":
+			r = make([]interface{}, 10)
+		case "string":
+			r = ""
+		}
+		if err := json.Unmarshal(b, &r); err != nil {
+			c.statusE = fmt.Sprintf("error: %s", err.Error())
+			return
+		}
+		buf := &bytes.Buffer{}
+		_, err := ntenc.Encode(r, buf)
+		if err != nil {
+			if c.contains("dump_err.json") {
+				c.statusE = "ok"
+				return
+			}
+			c.statusE = fmt.Sprintf("error: %s", err.Error())
+			c.isFail = true
+			return
+		}
+		c.statusE = "parsed"
+		if compareJson(buf, c, t) {
+			c.statusE = "ok"
+		}
+	}
+}
+
+func compareOutput(any interface{}, c *ntTestCase, t *testing.T) bool {
+	return true
+}
+
+func compareJson(buf *bytes.Buffer, c *ntTestCase, t *testing.T) bool {
+	return true
+}
+
+func listTestCases(t *testing.T) []ntTestCase {
+	list, err := ioutil.ReadDir(suitePath)
+	if err != nil {
+		t.Fatalf("unable to read test-suite dir")
+	}
+	t.Logf("%d test cases in NestedText Suite", len(list))
+	cases := make([]ntTestCase, len(list))
+	for i, caseDir := range list {
+		cases[i] = ntTestCase{
+			name: caseDir.Name(),
+			dir:  caseDir,
+		}
+	}
+	return cases
+}
